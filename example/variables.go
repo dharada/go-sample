9af17406e80870023e37ec70280c74c6ab990c9d
Daisuke Harada <dharada6@gmail.com>
2020-08-07 14:15:28 +0900

    update

--- example/variables.go
+++ example/variables.go
@@ -1,14 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main() {
+const s string = "constant"
 
-	var a = "initial"
+func main() {	var a = "initial"
 	fmt.Println(a)
 
 	var b, c int = 1, 2
 	fmt.Println(b, c)
 
-
+	fmt.Println(s)
+	const n = 6000
+	const d = 3e20 / n
+	fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
+	fmt.Println("math.Sin=" + fmt.Sprintf("%f", math.Sin(n)))
+	fmt.Println("math.Cos=" + fmt.Sprintf("%f", math.Cos(n)))
+	fmt.Println("math.Tan=" + fmt.Sprintf("%f", math.Tan(n)))
 }
