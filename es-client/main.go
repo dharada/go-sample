67b4a02195203fc8af1a9b0d1e401f2eb76a798d
dharada <dharada6@gmail.com>
2020-09-18 12:26:53 +0900

    update

---
+++ es-client/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esutil"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+
+	fmt.Println("version:", elasticsearch.Version)
+	fmt.Printf("indexer: %+v\n", esutil.BulkIndexerConfig{})
+
+	onCloud := true
+
+	if onCloud {
+
+		cfg := elasticsearch.Config{
+			CloudID: "v7-experience:ZXVyb3BlLXdlc3QxLmdjcC5jbG91ZC5lcy5pbyQ5ZjIyODNkOGY2ODg0ZWMyOTdlM2E2YzliN2Y5NzUzMCQ4NDdjZjVjZDg2MTA0Y2IyODBjMDAzNTM4M2YxOTc3MA==",
+			APIKey:  "bUpzd24zUUJlcnE0NEQ0VEhHWHM6YjMyYXpGajRTV0c0SThWc2dDeThIQQ==",
+		}
+
+		es, err := elasticsearch.NewClient(cfg)
+
+		res, err := es.Info()
+		if err != nil {
+			log.Fatalf("Error getting response: %s", err)
+		}
+		log.Println(res)
+
+	} else {
+
+		cert, _ := ioutil.ReadFile("/home/daisuke/d/7.9.1/elasticsearch/config/certs/ca.crt")
+
+		var (
+			clusterURLs = []string{"https://daisuke:9200"}
+			username    = "elastic"
+			password    = "changeme"
+		)
+
+		cfg := elasticsearch.Config{
+			Addresses: clusterURLs,
+			Username:  username,
+			Password:  password,
+			CACert: cert,
+		}
+
+		es, err := elasticsearch.NewClient(cfg)
+
+		res, err := es.Info()
+		if err != nil {
+			log.Fatalf("Error getting response: %s", err)
+		}
+
+		log.Println(res)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/go-samples
+
+go 1.15
+
+require github.com/elastic/go-elasticsearch/v7 v7.5.1-0.20200917075835-3a414481199c
